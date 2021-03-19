316a7d7fa207ad3676b6378d6d74e3d0b1228f23
lolkozolol <evgenij@MacBook-Air-Vladimir.local>
2021-03-19 06:29:47 +0300

    MeatClicker
    Eazy MeatClicker

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const button = document.querySelector('#finish')
+const score = document.querySelector('#score')
+const restart = document.querySelector('#restart')
+
+
+let count = 0 
+
+button.addEventListener('click', () => 
+    score.textContent = 
+    score.innerHtml =
+    count += 1);
+
+restart.addEventListener('click', () => 
+    score.textContent = 
+    score.innerHtml =
+    count = 0);
+
